3a76e58f5063cc3ee1c16add35179136da7d276f
James Summa <jamesdsumma@gmail.com>
2026-03-11 21:19:57 -0400

    base xml, activity, and fragment for home screen

---
+++ app/src/main/java/com/adelsi/cozycodex/ui/HomeFragment.kt
@@ -0,0 +1,12 @@
+package com.adelsi.cozycodex.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.adelsi.cozycodex.R
+
+class HomeFragment : Fragment(R.layout.fragment_home){
+
+}
